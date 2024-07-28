3190cd28a5e4cc676795542b5da86815c4f2f2b2
Aves Chan <aves.chan@yandex.ru>
2024-07-28 22:56:51 +0300

    fix

--- bot/run_bot.py
+++ bot/run_bot.py
@@ -51,9 +51,9 @@ setup_dialogs(dp)
 @dp.message(Command('start'))
 async def start(message: Message, dialog_manager: DialogManager):
     db_check_user(telegram_id=message.from_user.id,
-                  username=message.from_user.username,
-                  first_name=message.from_user.first_name,
-                  last_name=message.from_user.last_name)
+                  username=str(message.from_user.username),
+                  first_name=str(message.from_user.first_name),
+                  last_name=str(message.from_user.last_name))
     await dialog_manager.start(MainSG.main, mode=StartMode.RESET_STACK)
 
 # @dp.callback_query(F.data == 'main menu')
