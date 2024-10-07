3ccdd48bb5083efd744abd2fe8919618f452b8ed
Aves Chan <aves.chan@yandex.ru>
2024-10-07 17:22:31 +0300

    add button "Hide the reminder"

--- bot/run_bot.py
+++ bot/run_bot.py
@@ -3,14 +3,15 @@ import asyncio
 from logging.handlers import RotatingFileHandler
 
 from aiogram.fsm.storage.base import KeyBuilder, DefaultKeyBuilder
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 from redis import asyncio as redis_asyncio
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.fsm.storage.redis import RedisStorage
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup
 
-from aiogram_dialog import (DialogManager, setup_dialogs, StartMode)
+from aiogram_dialog import (DialogManager, setup_dialogs, StartMode, ShowMode)
 
 from bot.database.db_config import Base, db_engine
 from bot.database.db_question import db_get_all_tasks_for_remind, db_set_completed_remind, db_check_user
@@ -23,10 +24,10 @@ from config import BOT_TOKEN, REDIS_PORT, REDIS_PASSWORD, REDIS_HOST
 
 import logging
 
-# def kb_back() -> InlineKeyboardMarkup:
-#     kb = InlineKeyboardBuilder()
-#     kb.button(text='main menu', callback_data='main menu')
-#     return kb.as_markup()
+def kb_back() -> InlineKeyboardMarkup:
+    kb = InlineKeyboardBuilder()
+    kb.button(text='Hide the reminder', callback_data='hide the reminder')
+    return kb.as_markup()
 
 
 async def update_remind() -> None:
@@ -39,7 +40,7 @@ async def update_remind() -> None:
             if min_time[0] < datetime.datetime.now():
                 await bot.send_message(chat_id=min_time[1][1].telegram_id,
                                        text=f'<b>{min_time[1][1].title}</b>\n\n{min_time[1][1].description}',
-                                       # reply_markup=kb_back(),
+                                       reply_markup=kb_back(),
                                        parse_mode='HTML')
                 db_set_completed_remind(min_time[1][1].telegram_id, min_time[1][1].title)
         await asyncio.sleep(0.5)
@@ -59,9 +60,10 @@ async def start(message: Message, dialog_manager: DialogManager):
                   last_name=str(message.from_user.last_name))
     await dialog_manager.start(MainSG.main, mode=StartMode.RESET_STACK)
 
-# @dp.callback_query(F.data == 'main menu')
-# async def start(cd: CallbackQuery, dialog_manager: DialogManager):
-#     await dialog_manager.start(MainSG.main, mode=StartMode.RESET_STACK)
+@dp.callback_query(F.data == 'hide the reminder')
+async def handler_hide_the_reminder(cd: CallbackQuery, dialog_manager: DialogManager):
+    await cd.message.delete()
+    await cd.answer()
 
 async def main():
     Base.metadata.create_all(bind=db_engine)
